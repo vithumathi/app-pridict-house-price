7b21169d8d63655dc185285efaa0f021e72ed30a
Vithu Mathiaparanam <vithu267@hotmail.com>
2020-07-06 19:58:47 -0400

    added publish docket image & run kubernetes scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="vithum/app-ml-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-micros-api\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-micros-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-micros-api 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="vithum/app-ml-api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag app-ml-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
